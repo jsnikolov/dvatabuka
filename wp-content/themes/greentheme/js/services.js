116eaba91f71f98539862a2105b0f8a5bd014b5e
jsnikolov <jsnikolov@gmail.com>
2025-06-24 22:17:36 +0300

    Промяна в services.js

--- wp-content/themes/greentheme/js/services.js
+++ wp-content/themes/greentheme/js/services.js
@@ -8,4 +8,34 @@ function gtag(){dataLayer.push(arguments)}window.dataLayer=window.dataLayer||[],
  * AdSense
  * Greentheme JS Functions
  */
+/*
 !function(o){o(".btn").click(function(){o("#nav").toggleClass("toggle")}),o("#book-cover .book-cover").hover(function(){o(".cover-title",this).slideToggle("200")}),o(window).resize(function(){o(window).width()>900&&o("#nav").removeClass("toggle"),o(".cover-title").removeAttr("style")}),o(".adsbygoogle").each(function(){(adsbygoogle=window.adsbygoogle||[]).push({})})}(jQuery);
+*/
+
+!function(o) {
+  o(".btn").click(function() {
+    o("#nav").toggleClass("toggle");
+  });
+
+  o("#book-cover .book-cover").hover(function() {
+    o(".cover-title", this).slideToggle("200");
+  });
+
+  o(window).resize(function() {
+    o(window).width() > 900 && o("#nav").removeClass("toggle");
+    o(".cover-title").removeAttr("style");
+  });
+
+  function initAds() {
+    if (typeof adsbygoogle === "undefined") {
+      setTimeout(initAds, 100);
+      return;
+    }
+    o(".adsbygoogle").each(function() {
+      (adsbygoogle = window.adsbygoogle || []).push({});
+    });
+  }
+
+  initAds();
+}(jQuery);
+
