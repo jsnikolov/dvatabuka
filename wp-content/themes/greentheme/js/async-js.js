584e62af3a450114d2645e24b2b22652e574473a
jsnikolov <jsnikolov@gmail.com>
2025-06-29 17:50:43 +0300

    -

--- wp-content/themes/greentheme/js/async-js.js
+++ wp-content/themes/greentheme/js/async-js.js
@@ -1,13 +1,4 @@
-// 1. Създаване на скрипт
-function createScript(src) {
-    const s = document.createElement("script");
-    s.type = "text/javascript";
-    s.async = true;
-    s.src = src;
-    return s;
-}
-
-// 2. CookieYes
+// 1. CookieYes
 function createCookieYesScript(src) {
     var element = document.createElement("script");
     element.id = "cookieyes";
@@ -18,24 +9,18 @@ function createCookieYesScript(src) {
 document.head.appendChild(createCookieYesScript("https://cdn-cookieyes.com/client_data/48089ea18522c94f7a3cc421/script.js"));
 
 
-
-//function loadCookieYes() {
-//    var element = document.createElement("script");
-//    element.id = "cookieyes";
-//    element.type = "text/javascript";
-//    element.src = "https://cdn-cookieyes.com/client_data/48089ea18522c94f7a3cc421/script.js";
-//    document.head.appendChild(script);
-//}
-//loadCookieYes();
-
-
-
-
-
+// 2. Създаване на скрипт
+function createScript(src){
+	var element = document.createElement("script");
+	element.type = "text/javascript";
+	element.async = true;
+	element.src = src;
+	return element;
+}
 
 
 // 3. Категоризирани скриптове
-const scriptsByCategory = {
+var scriptsByCategory = {
     analytics: [
         createScript("https://www.googletagmanager.com/gtag/js?id=UA-58102164-1"),
         createScript("https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js")
@@ -50,17 +35,19 @@ const scriptsByCategory = {
 
 // 4. GTM
 function insertGTMscript() {
-    const script = document.createElement('script');
+    var script = document.createElement('script');
     script.async = true;
+
     script.text = "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':" +
         "new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0]," +
         "j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=" +
         "'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);" +
         "})(window,document,'script','dataLayer','GTM-MSK9ZKQV');";
+
     document.head.appendChild(script);
 }
 function insertGTMiframe() {
-    const iframe = document.createElement('iframe');
+    var iframe = document.createElement('iframe');
     iframe.src = "https://www.googletagmanager.com/ns.html?id=GTM-MSK9ZKQV";
     iframe.height = "0";
     iframe.width = "0";
@@ -71,7 +58,7 @@ function insertGTMiframe() {
 
 // 5. Зареждане според съгласие
 function loadScriptsBasedOnConsent() {
-    const consent = window.CookieYes?.getUserConsent?.();
+    var consent = window.CookieYes?.getUserConsent?.();
     if (!consent) return;
 
     if (consent.analytics) {
