0131ee53a1d5d2cb3f832a4fd6ead5e9863f24ad
jsnikolov <jsnikolov@gmail.com>
2025-06-29 19:09:18 +0300

    -

--- wp-content/themes/greentheme/js/async-js.js
+++ wp-content/themes/greentheme/js/async-js.js
@@ -1,53 +1,56 @@
-// 1. CookieYes
-function createCookieYesScript(src) {
-    var element = document.createElement("script");
-    element.id = "cookieyes";
-    element.type = "text/javascript";
-    element.src = src;
-    return element;
-}
-document.head.appendChild(createCookieYesScript("https://cdn-cookieyes.com/client_data/48089ea18522c94f7a3cc421/script.js"));
-
-
-// 2. Създаване на скрипт
-function createScript(src){
-	var element = document.createElement("script");
-	element.type = "text/javascript";
-	element.async = true;
-	element.src = src;
-	return element;
+// Помощна функция за прочитане на бисквитка по име
+function getCookie(name) {
+    const match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
+    return match ? decodeURIComponent(match[2]) : null;
 }
 
+// Взимаме и парсваме съгласието от cookieyes-consent
+function getConsentFromCookie() {
+    const cookie = getCookie('cookieyes-consent');
+    if (!cookie) return null;
 
-// 3. Категоризирани скриптове
-var scriptsByCategory = {
+    try {
+        return JSON.parse(cookie);
+    } catch (e) {
+        return null;
+    }
+}
+
+// Функция за създаване на скрипт елемент
+function createScript(src, async = true) {
+    const s = document.createElement('script');
+    s.type = "text/javascript";
+    s.async = async;
+    s.src = src;
+    return s;
+}
+
+// Скриптове по категории
+const scriptsByCategory = {
     analytics: [
-        createScript("https://www.googletagmanager.com/gtag/js?id=UA-58102164-1"),
-        createScript("https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js")
+        () => createScript("https://www.googletagmanager.com/gtag/js?id=UA-58102164-1"),
+        () => createScript("https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js")
     ],
     social: [
-        createScript(window.location.origin + "/wp-content/themes/greentheme/js/fb-min.js")
+        () => createScript(window.location.origin + "/wp-content/themes/greentheme/js/fb-min.js")
     ],
     functional: [
-        createScript(window.location.origin + "/wp-content/themes/greentheme/js/services.js")
+        () => createScript(window.location.origin + "/wp-content/themes/greentheme/js/services.js")
     ]
 };
 
-// 4. GTM
-function insertGTMscript() {
-    var script = document.createElement('script');
-    script.async = true;
-
-    script.text = "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':" +
+// Зареждаме GTM скрипта и iframe, ако има consent.analytics
+function insertGTM() {
+    const gtmScript = document.createElement('script');
+    gtmScript.async = true;
+    gtmScript.text = "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':" +
         "new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0]," +
         "j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=" +
         "'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);" +
         "})(window,document,'script','dataLayer','GTM-MSK9ZKQV');";
+    document.head.appendChild(gtmScript);
 
-    document.head.appendChild(script);
-}
-function insertGTMiframe() {
-    var iframe = document.createElement('iframe');
+    const iframe = document.createElement('iframe');
     iframe.src = "https://www.googletagmanager.com/ns.html?id=GTM-MSK9ZKQV";
     iframe.height = "0";
     iframe.width = "0";
@@ -56,19 +59,21 @@ function insertGTMiframe() {
     document.body.insertBefore(iframe, document.body.firstChild);
 }
 
-// 5. Зареждане според съгласие
+// Основна функция за зареждане според consent
 function loadScriptsBasedOnConsent() {
-    var consent = window.CookieYes?.getUserConsent?.();
+    const consent = getConsentFromCookie();
     if (!consent) return;
 
+    // GTM (ако има съгласие за analytics)
     if (consent.analytics) {
-        insertGTMscript();
-        insertGTMiframe();
+        insertGTM();
     }
 
-    Object.entries(scriptsByCategory).forEach(([category, scripts]) => {
+    // Зареждаме останалите скриптове по категории
+    Object.entries(scriptsByCategory).forEach(([category, scriptCreators]) => {
         if (consent[category]) {
-            scripts.forEach(script => {
+            scriptCreators.forEach(createFn => {
+                const script = createFn();
                 if (category === "social") {
                     document.body.appendChild(script);
                 } else {
@@ -79,7 +84,8 @@ function loadScriptsBasedOnConsent() {
     });
 }
 
-// 6. Изчакваме CookieYes да е готов
-window.addEventListener("cookieyes_ready", loadScriptsBasedOnConsent);
-// При промяна на съгласие
-window.addEventListener("cookieyes_consent_update", loadScriptsBasedOnConsent);
+// Изчакваме пълно зареждане на страницата
+window.addEventListener('load', function () {
+    // Даваме 0.8-1 секунда да се зададе бисквитката cookieyes-consent
+    setTimeout(loadScriptsBasedOnConsent, 900);
+});
